3ecbb12b126e9aed201b79ddcbfb0817b2037215
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-05-28 12:34:41 +0700

    <Feature> [User]: User register sudah di cek memakai supertest dan berjalan skema sesuai integration test

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { publicRouter } from "../view/route/public-api";
+import { errorMiddleware } from "../middlware/error-middleware";
 
 export const web = express()
 web.use(express.json())
+web.use(publicRouter)
+web.use(errorMiddleware)
