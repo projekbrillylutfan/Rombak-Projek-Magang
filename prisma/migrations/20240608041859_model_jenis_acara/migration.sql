93d8888542d39984bf8446d4ebea19166220c66b
projekbrillylutfan <projekbrillylutfan@gmail.com>
2024-06-08 11:20:30 +0700

    <Model DB> [Jenis Acara] : pembuatan model DB untuk jenis acara

---
+++ prisma/migrations/20240608041859_model_jenis_acara/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `jenis_acara` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `jenis_acara` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
